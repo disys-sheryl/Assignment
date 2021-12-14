7de82856a74d0e0e025b8e68a23b41732ca522bb
disys-sheryl <95470429+disys-sheryl@users.noreply.github.com>
2021-12-14 01:22:55 -0700

    Add files via upload

---
+++ gpay.py
@@ -0,0 +1,147 @@
+
+
+Sheryl=gp.Google_pay("Sherylrajakumar@gmail.com","7904125152","SherylCatherine")
+Sheryl.open_gpay()
+Sheryl.email_verification()
+Sheryl.mobile_verification()
+Sheryl.name_verification()
+Sheryl.otp_verification(25433,25433)
+Sheryl.Bank_verification()
+Sheryl.PanCard_Verification()
+Sheryl.set_Pin("7176")
+Sheryl.Enter_your_Pin(3465,3465)
+
+class Phone_pe(gp.Google_pay):                                                                                  
+    def __init__(self,Email_id,Phone_number,Name):
+        super().__init__(Email_id,Phone_number,Name)
+
+    def open_phonepe(self):
+        print("Phone pe")
+        
+Hope=Phone_pe("sheryl.cathy@gmail.com","9003085347","Sheryl")
+Hope.open_phonepe()
+Hope.mobile_verification()
+Hope.name_verification()
+Hope.otp_verification(780965,780965)
+Hope.Bank_verification()
+Hope.PanCard_Verification()
+Hope.set_Pin("676132")
+Hope.Enter_your_Pin(4576,4576)
+
+
+        
+googlepay=[{"name":"abitha","gpaynum":7397266887,"type":"personal","transaction":"regular"},                       
+           {"name":"aishwarya","gpaynum":7305341565,"type":"personal","transaction":"regular"},
+           {"name":"ajit","gpaynum":6381347949,"type":"personal","transaction":"rare"},
+           {"name":"anand","gpaynum":7358355054,"type":"personal","transaction":"never"},
+           {"name":"angel","gpaynum":7200636126,"type":"personal","transaction":"rare"},
+           {"name":"annie","gpaynum":9597916931,"type":"personal","transaction":"rare"},
+           {"name":"aswin","gpaynum":8056469214,"type":"personal","transaction":"regular"},
+           {"name":"ayarin","gpaynum":9962454833,"type":"personal","transaction":"rare"},
+           {"name":"ayesha","gpaynum":8015341851,"type":"personal","transaction":"rare"},
+           {"name":"baby nivetha","gpaynum":7305624091,"type":"personal","transaction":"rare"},
+           {"name":"balaji","gpaynum":8939509826,"type":"personal","transaction":"rare"},
+           {"name":"banu","gpaynum":6369121983,"type":"personal","transaction":"regular"},
+           {"name":"benedict","gpaynum":9833807044,"type":"personal","transaction":"regular"},
+           {"name":"beni","gpaynum":9941297487,"type":"personal","transaction":"rare"},
+           {"name":"benita","gpaynum":7200001990,"type":"personal","transaction":"regular"},
+           {"name":"benny","gpaynum":6382880108,"type":"personal","transaction":"rare"},
+           {"name":"betsy","gpaynum":9941656319,"type":"personal","transaction":"regular"},
+           {"name":"blessy","gpaynum":9500075619,"type":"personal","transaction":"rare"},
+           {"name":"brindha","gpaynum":8072512570,"type":"personal","transaction":"regular"},
+           {"name":"caesy","gpaynum":6382761961,"type":"personal","transaction":"rare"},
+           {"name":"caleb","gpaynum":9791045340,"type":"personal","transaction":"regular"},
+           {"name":"dean","gpaynum":9841032642,"type":"personal","transaction":"regular"},
+           {"name":"derrick","gpaynum":6383908036,"type":"personal","transaction":"regular"},
+           {"name":"divya","gpaynum":8248631340,"type":"personal","transaction":"rare"},
+           {"name":"dulip","gpaynum":6374339918,"type":"personal","transaction":"regular"},
+           {"name":"eddie","gpaynum":9840644601,"type":"personal","transaction":"regular"},
+           {"name":"emima","gpaynum":6379741175,"type":"personal","transaction":"regular"},
+           {"name":"esther","gpaynum":8124252926,"type":"personal","transaction":"regular"},
+           {"name":"fanny","gpaynum":9176093745,"type":"personal","transaction":"regular"},
+           {"name":"gawtam","gpaynum":9176358088,"type":"personal","transaction":"regular"},
+           {"name":"gladson","gpaynum":7305331140,"type":"personal","transaction":"rare"},
+           {"name":"gokul","gpaynum":6384316771,"type":"personal","transaction":"regular"},
+           {"name":"gracy","gpaynum":9150381712,"type":"personal","transaction":"regular"},
+           {"name":"hena","gpaynum":9003037517,"type":"personal","transaction":"regular"},
+           {"name":"hephzibah","gpaynum":9515605233,"type":"personal","transaction":"regular"},
+           {"name":"ida","gpaynum":9791143754,"type":"personal","transaction":"regular"},
+           {"name":"indu","gpaynum":8056265597,"type":"personal","transaction":"regular"},
+           {"name":"ivin","gpaynum":7010469081,"type":"personal","transaction":"regular"},
+           {"name":"jaanu","gpaynum":6379840455,"type":"personal","transaction":"rare"},
+           {"name":"jaaziel","gpaynum":9791280444,"type":"personal","transaction":"regular"},
+           {"name":"jacinth ","gpaynum":8667639927,"type":"personal","transaction":"rare"},
+           {"name":"jacob","gpaynum":8667464628,"type":"personal","transaction":"regular"},
+           {"name":"jagan","gpaynum":9962821791,"type":"personal","transaction":"regular"},
+           {"name":"jasper","gpaynum":9551852580,"type":"personal","transaction":"rare"},
+           {"name":"jeba","gpaynum":8939475795,"type":"personal","transaction":"regular"},
+           {"name":"jeevitha","gpaynum":7338935895,"type":"personal","transaction":"rare"},
+           {"name":"jena","gpaynum":9444919116,"type":"personal","transaction":"regular"},
+           {"name":"jeni","gpaynum":8531025248,"type":"personal","transaction":"regular"},
+           {"name":"jessy","gpaynum":9789869415,"type":"personal","transaction":"regular"},
+           {"name":"jeya singh","gpaynum":9677150962,"type":"personal","transaction":"regular"},
+           {"name":"john","gpaynum":9003899016,"type":"personal","transaction":"rare"},
+           {"name":"josh","gpaynum":9361486028,"type":"personal","transaction":"regular"},
+           {"name":"jona","gpaynum":6382826039,"type":"personal","transaction":"regular"},
+           {"name":"julian","gpaynum":9080995159,"type":"personal","transaction":"regular"},
+           {"name":"juliet","gpaynum":8220584872,"type":"personal","transaction":"rare"},
+           {"name":"justin","gpaynum":9710405288,"type":"personal","transaction":"rare"},
+           {"name":"karthi","gpaynum":8778799456,"type":"personal","transaction":"regular"},
+           {"name":"kavitha","gpaynum":9840042296,"type":"personal","transaction":"regular"},
+           {"name":"keerthi","gpaynum":7305066119,"type":"personal","transaction":"rare"},
+           {"name":"kingston","gpaynum":8754554295,"type":"personal","transaction":"regular"},
+           {"name":"kiruba","gpaynum":6383634327,"type":"personal","transaction":"rare"},
+           {"name":"kishore","gpaynum":9789029339,"type":"personal","transaction":"regular"},
+           {"name":"kowsalya","gpaynum":7871125838,"type":"personal","transaction":"regular"},
+           {"name":"keerthana","gpaynum":7448744931,"type":"personal","transaction":"rare"},
+           {"name":"lkee","gpaynum":7358565943,"type":"personal","transaction":"regular"},
+           {"name":"lincy","gpaynum":7305772128,"type":"personal","transaction":"regular"},
+           {"name":"linda","gpaynum":7358479530,"type":"personal","transaction":"regular"},
+           {"name":"madhu","gpaynum":6383188073,"type":"personal","transaction":"regular"},
+           {"name":"maha","gpaynum":9940235467,"type":"personal","transaction":"rare"},
+           {"name":"manoj","gpaynum":7358602342,"type":"personal","transaction":"regular"},
+           {"name":"merlin","gpaynum":9500771102,"type":"personal","transaction":"regular"},
+           {"name":"michael","gpaynum":9092042867,"type":"personal","transaction":"regular"},
+           {"name":"milne","gpaynum":9445891948,"type":"personal","transaction":"regular"},
+           {"name":"monica","gpaynum":8754348244,"type":"personal","transaction":"rare"},
+           {"name":"pavithra","gpaynum":7395949407,"type":"personal","transaction":"regular"},
+           {"name":"pradeep","gpaynum":9751783763,"type":"personal","transaction":"regular"},
+           {"name":"prem","gpaynum":7358160847,"type":"personal","transaction":"regular"},
+           {"name":"presley","gpaynum":7550228649,"type":"personal","transaction":"regular"},
+           {"name":"priya","gpaynum":9620423983,"type":"personal","transaction":"rare"},
+           {"name":"puppy","gpaynum":8526264054,"type":"personal","transaction":"rare"},
+           {"name":"rahul","gpaynum":6381281050,"type":"personal","transaction":"regular"},
+           {"name":"raja","gpaynum":9840696542,"type":"personal","transaction":"regular"},
+           {"name":"reji","gpaynum":8124263483,"type":"personal","transaction":"regular"},
+           {"name":"reni","gpaynum":63830035506,"type":"personal","transaction":"regular"},
+           {"name":"revathi","gpaynum":9094639197,"type":"personal","transaction":"regular"},
+           {"name":"roseline","gpaynum":9150544355,"type":"personal","transaction":"rare"},
+           {"name":"sam","gpaynum":7806906745,"type":"personal","transaction":"regular"},
+           {"name":"sameera","gpaynum":9150961045,"type":"personal","transaction":"regular"},
+           {"name":"sammy","gpaynum":9676520865,"type":"personal","transaction":"regular"},
+           {"name":"sandhya","gpaynum":7358470300,"type":"business","transaction":"rare"},
+           {"name":"sandra","gpaynum":8838105667,"type":"personal","transaction":"regular"},
+           {"name":"sankar","gpaynum":6369772429,"type":"personal","transaction":"regular"},
+           {"name":"sarah","gpaynum":9790830981,"type":"personal","transaction":"regular"},
+           {"name":"saro","gpaynum":9489300189,"type":"personal","transaction":"regular"},
+           {"name":"sathya","gpaynum":8610129526,"type":"personal","transaction":"regular"},
+           {"name":"selvaraj","gpaynum":7397347695,"type":"personal","transaction":"rare"},
+           {"name":"selvin","gpaynum":9500079580,"type":"personal","transaction":"regular"},
+           {"name":"shakthi","gpaynum":7708374654,"type":"personal","transaction":"regular"},
+           {"name":"shalu","gpaynum":9344028505,"type":"personal","transaction":"regular"},
+           {"name":"sharmi","gpaynum":6369143345,"type":"personal","transaction":"rare"},
+           {"name":"sharon","gpaynum":9894190843,"type":"personal","transaction":"regular"},
+           {"name":"shiny","gpaynum":7401790602,"type":"personal","transaction":"regular"},
+           {"name":"sneha","gpaynum":9840430478,"type":"personal","transaction":"regular"},
+           {"name":"soniya","gpaynum":9840860014,"type":"personal","transaction":"rare"},
+           {"name":"stephen","gpaynum":7448729790,"type":"personal","transaction":"regular"},
+           {"name":"stephy","gpaynum":6380874698,"type":"personal","transaction":"rare"},
+           {"name":"subhi","gpaynum":9361447138,"type":"personal","transaction":"regular"},
+           {"name":"sujitha","gpaynum":7358701090,"type":"personal","transaction":"regular"},
+           {"name":"swathi","gpaynum":6382718022,"type":"personal","transaction":"regular"}]
+          
+for i in googlepay:                                                                                                             
+    for j,k in i.items():                                                                                                   
+        print(f"{j}:{k}")
+    
+
